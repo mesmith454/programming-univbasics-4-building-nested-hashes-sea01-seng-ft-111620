fb04fa10e1a3bb8cca010d600ad54311be5e5a25
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-01 20:26:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,9 +6,8 @@ def base_hash
 end
 
 def monopoly_with_second_tier
- base_hash = 
+ base_hash[:railroads] 
  {
- :railroads =>
     :pieces =>4,
     :rent_in_dollars => {},
     :names => {}
@@ -34,7 +33,11 @@ def monopoly_with_fourth_tier
   base_hash[:railroads]
   {
     :pieces =>"4",
-    :rent_in_dollars => {:one_piece_owned => "25", :two_pieces_owned => "50", :three_pieces_owned => "100", :four_pieces_owned => "200"},
+    :rent_in_dollars => 
+      {
+        :one_piece_owned => "25", :two_pieces_owned => "50", :three_pieces_owned => "100", :four_pieces_owned => "200"
+        
+      },
     :names => 
     {
       :reading_railroad => {:mortgage_value => "100"},
@@ -43,5 +46,4 @@ def monopoly_with_fourth_tier
       :shortline_railroad => {:mortgage_value => "800"}
     }
   }
-
 end
